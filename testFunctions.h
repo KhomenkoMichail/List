572e84cf65da1fd62b59c271b165f014c5c20029
Lemming_S_Maslinoi <130701315+KhomenkoMichail@users.noreply.github.com>
2026-04-07 18:37:40 +0300

    Delete testFunctions.h

--- testFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_FUNCTIONS_H
-#define TEST_FUNCTIONS_H
-
-double classicListTimetest (struct classicList* cList, struct dump* dumpInfo);
-
-double hashFriendlyListTimetest (struct list* lst, struct dump* dumpInfo);
-
-#endif
